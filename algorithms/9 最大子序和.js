6fd5573b744a4b071bfab1b8d7189f49d1ccbfcb
lizhiyuan <148334733@qq.com>
2019-07-06 17:04:14 +0800

    还是没有感觉....

--- algorithms/9 最大子序和.js
+++ algorithms/9 最大子序和.js
@@ -3,5 +3,15 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    
+    let ans = nums[0];
+    let sum = 0;
+    for(const num of nums){
+        if(sum > 0){
+            sum += num;
+        }else{
+            sum = num;
+        }
+        ans = Math.max(ans,sum);
+    }
+    return ans;
 };
