4f3f1f1a7bf652c088835d94eaea3b0a34799af3
lizhiyuan <148334733@qq.com>
2019-07-20 18:30:19 +0800

    重来推导...

--- algorithms/11 回文数.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
-    var str = x.toString();
-    var newStr = str.split('').reverse().join('');
-    return str === newStr
-};
