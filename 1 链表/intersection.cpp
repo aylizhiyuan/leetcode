c8e54cdf4f97e0ec26d6e8f669c13ea01448426b
李志远 <lizhiyuan@lizhiyuandeMacBook-Pro.local>
2019-08-10 14:42:10 +0800

    今天完成链表即可....

--- 1 链表/intersection.cpp
+++ 1 链表/intersection.cpp
@@ -16,13 +16,32 @@ int get_list_length(ListNode *head){
 };
 //将指针向前移动至多出节点个数后面的位置
 ListNode *foward_long_list(int long_len,int short_len,ListNode *head){
-
+    int delta = long_len - short_len;
+    while(head && delta){
+        head = head->next;
+        delta--;
+    }
+    return head;
 };
 
 class Solution {
     public: 
         ListNode *getIntersectionNode(ListNode *headA,ListNode *headB){
-        
+            int list_A_len = get_list_length(headA);
+            int list_B_len = get_list_length(headB);
+            if(list_A_len > list_B_len){
+                headA = foward_long_list(list_A_len,list_B_len,headA);
+            }else{
+                headB = foward_long_list(list_B_len,list_A_len,headB);
+            }
+            while(headA && headB){
+                if(headA == headB){
+                    return headA;
+                }
+                headA = headA->next;
+                headB = headB->next;
+            }
+            return NULL;
         }  
 };
 int main(){
---
+++ 1 链表/partition.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <set> //STL set的使用
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode(int x):val(x),next(NULL){};
+};
+int main(){
+    ListNode a(1);
+    
+}
