6550ac3f2fe1846f78ee083de21a2f013eb2cd36
lizhiyuan <148334733@qq.com>
2019-08-12 13:49:52 +0800

    搞定贪心...

---
+++ 3 贪心算法/removeKdigits.cpp
@@ -0,0 +1,12 @@
+//移除k个数字
+#include <stdio.h>
+#include <string>
+class Solution {
+    public: 
+        std::string removeKdigits(std::string nums,int k){
+
+        }        
+};
+int main(){
+
+}
