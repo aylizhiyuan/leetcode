7ee4dcb5e679dc3266e9b0a858a3f5ebc3ccffac
lizhiyuan <148334733@qq.com>
2019-08-17 15:38:54 +0800

    二分查找今天必须整完..

---
+++ 6 二叉排序树/insertPostion.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <vector>
+class Solution {
+    public: 
+        int searchInsert(std::vector<int> &nums,int target){
+            int index = -1; //最终返回的下标，若找到则为该元素的下标，否则为需要插入的位置
+            int begin  = 0; //搜索区间左端点
+            int end = nums.size() - 1;
+            while(index == -1){
+                
+            }
+        }   
+}
