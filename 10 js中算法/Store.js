62d97917097ecfda96577b879fcd8a5d448ce686
lizhiyuan <148334733@qq.com>
2019-08-31 17:41:17 +0800

    基本上搞定了...哈哈！！！

--- 10 js中算法/Store.js
+++ 10 js中算法/Store.js
@@ -5,17 +5,154 @@ const _forEach = (obj,callback)=>{
     })
 }
 class ModuleConnection {
+    constructor(options){
+        this.register([],options);
+    }
+    register(path,rootModule){
+        let newModule = {
+            _raw:rootModule,
+            _children:{},
+            state:rootModule.state
+        }
+        if(path.length === 0){
+            this.root = newModule; //注册根模块，一开始传进来的模块
+        }else{
+            path.slice(0,-1).reduce((root,current)=>{
+                return this.root._children[current];
+            },this.root);
+            this.root._children[path[path.length-1]] = newModule;
+        }
+        //如果这个模块还有其他的子模块的话，递归注册模块
+        if(rootModule.modules){
+            _forEach(rootModule.modules,(moduleName,module)=>{
+                this.register(path.concat(moduleName),module)
+            })
+        }
+    }
 
 }
 const installModule = (store,state,path,rootModule)=>{
+    if(path.length > 0) {
+        let parent = path.slice(0, -1).reduce((state, current)=>{
+            return state[current]
+        }, state);
+        Vue.set(parent, path[path.length - 1], rootModule.state);
+    }
 
+
+    let getters = rootModule._raw.getters
+    if (getters) { // 
+        _forEach(getters, (getterName, fn) => {
+            Object.defineProperty(store.getters, getterName, {
+                get: () => {
+                    return fn(rootModule.state);
+                }
+            })
+        })
+    }
+
+    let mutations = rootModule._raw.mutations;
+    if(mutations) {
+        _forEach(mutations, (mutationName, fn) => {
+            let arr = store.mutations[mutationName] || (store.mutations[mutationName] = []);
+            arr.push((payload) => {
+                fn(rootModule.state, payload);
+            });
+        }) 
+    }
+
+    let actions = rootModule._raw.actions;
+    if(actions) {
+        _forEach(actions, (actionsName, fn) => {
+            let arr = store.actions[actionsName] || (store.actions[actionsName]= []);
+            arr.push((payload) => {
+                fn(store, payload);
+            });
+        }) 
+    }
+
+    _forEach(rootModule._children, (moduleName, module) => {
+        installModule(store, state, path.concat(moduleName), module);
+    })
 }
 class Store {
+    constructor(options){
+        this._s = new Vue({
+            data(){
+                return {
+                    state:options.state //把state变成可监控的对象
+                }
+            }
+        });
+    this.getters = {};
+    this.mutation = {};
+    this.actions = {};
+    // 把getters属性定义到 this.getters中，并根据状态的变化，重新执行此函数
+        // _forEach(getters, (getterName, value) => {
+        //     Object.defineProperty(this.getters, getterName, {
+        //         get: () => {
+        //             return value(this.state)
+        //         },
+        //         enumerable: true 
+        //     });
+        // });
+
+        // let mutations = options.mutations || {};
+        // // this.mutations = {};
+        // _forEach(mutations, (mutationName, value) => {
+        //     // 先把用户传过来的mutation放到store上
+        //     this.mutations[mutationName] = (payload) => {
+        //         value.call(this, this.state, payload);
+        //     }
+        // })
+
+        // let actions = options.actions || {};
+        // // this.actions = {};
+        // _forEach(actions, (actionName, value) => {
+        //     this.actions[actionName] = (payload) => {
+        //         value.call(this, this, payload);
+        //     }
+        // })
+    //vuex模块化
+    this.modules = new ModuleConnection(options);
+    installModule(this,this.state,[],this.modules.root);   
+    }
+    commit = (type,payload)=>{
+        this.mutation[type].forEach(fn=>fn(payload));
+    }
+    dispatch = (type,payload)=>{
+        this.actions[type].forEach(fn=>fn(payload));
+    }
+    get state(){
+        return this._s.state;
+    }
 
 }
 const install = (_Vue)=>{
-
+    Vue = _Vue;
+    Vue.mixin({
+        beforeCreate(){
+            //需要给每个组件都注册一个this.$store的属性
+            //需要判断是父组件还是子组件，如果是子组件，应该吧父组件的store增加给子组件
+            if(this.$options && this.$options.store){
+                this.$store = this.$options.store;
+            }else{
+                this.$store = this.$parent && this.$parent.$store;
+            }
+        }
+    })
 }
+//使用的时候是这样的
+// export default new Vuex.Store({
+//     modules:{},
+//     state:{},
+//     actions:{},
+//     getters:{},
+//     mutations:{}
+// })
+// new Vue({
+//     store
+// })
 export default {
     install,
     Store
--- 10 js中算法/router.js
+++ 10 js中算法/router.js
@@ -1,40 +1,201 @@
-//hash模式的跳转
-window.addEventListener('hashchange', () => {
-    this.transitionTo(this.getCurrentLocation())
-})
+//举例一个hash的模式
+export class Base {
 
-function getCurrentLocation() {
-    const href = window.location.href
-    const index = href.indexOf('#')
-    return index === -1 ? '' : href.slice(index + 1)
-}
-function changeUrl(path, replace) {
-    const href = window.location.href
-    const i = href.indexOf('#')
-    const base = i >= 0 ? href.slice(0, i) : href
-    if (replace) {
-      window.history.replaceState({}, '', `${base}#/${path}`)
-    } else {
-      window.history.pushState({}, '', `${base}#/${path}`)
+  constructor (router) {
+    this.router = router
+    this.current = {
+      path: '/',
+      query: {},
+      params: {},
+      name: '',
+      fullPath: '/',
+      route: {}
     }
-}
-//HTML5模式的跳转
-window.addEventListener('popstate', () => {
-    this.transitionTo(getLocation())
-})
+  }
 
-function getLocation (base = ''){
-    let path = window.location.pathname
-    if (base && path.indexOf(base) === 0) {
-      path = path.slice(base.length)
-    }
-    return (path || '/') + window.location.search + window.location.hash
-}
-  
-function changeUrl(base, path, replace) {
-    if (replace) {
-        window.history.replaceState({}, '', (base + path).replace(/\/\//g, '/'))
+
+  /**
+   * 路由转换
+   * @param target 目标路径
+   * @param cb 成功后的回调
+   */
+  transitionTo(target, cb) {
+    // 通过对比传入的 routes 获取匹配到的 targetRoute 对象
+    //注意，这里无需触发什么，只需要改变current的值，视图会更新的，如果是代码形式的
+    //需要主动调用push方法或者replace方法....
+    const targetRoute = match(target, this.router.routes)
+    this.confirmTransition(targetRoute, () => {
+      this.current.route = targetRoute
+      this.current.name = targetRoute.name
+      this.current.path = targetRoute.path
+      this.current.query = targetRoute.query || getQuery()
+      this.current.fullPath = getFullPath(this.current)
+      cb && cb()
+    })
+  }
+
+  /**
+   * 确认跳转
+   * @param route
+   * @param cb
+   */
+  confirmTransition (route, cb) {
+    // 钩子函数执行队列
+    let queue = [].concat(
+      this.router.beforeEach,
+      this.current.route.beforeLeave,
+      route.beforeEnter,
+      route.afterEnter
+    )
+
+    // 通过 step 调度执行
+    let i = -1
+    const step = () => {
+      i ++
+      if (i > queue.length) {
+        cb()
+      } else if (queue[i]) {
+        queue[i](step)
       } else {
-        window.history.pushState({}, '', (base + path).replace(/\/\//g, '/'))
-      }  
+        step()
+      }
+
+    }
+    step(i)
+  }
 }
+
+function getFullPath ({ path, query = {}, hash = '' }, _stringifyQuery){
+  const stringify = _stringifyQuery || stringifyQuery
+  return (path || '/') + stringify(query) + hash
+}
+
+export function match(path, routeMap) {
+  let match = {}
+  if (typeof path === 'string' || path.name === undefined) {
+    for(let route of routeMap) {
+      if (route.path === path || route.path === path.path) {
+        match = route
+        break;
+      }
+    }
+  } else {
+    for(let route of routeMap) {
+      if (route.name === path.name) {
+        match = route
+        if (path.query) {
+          match.query = path.query
+        }
+        break;
+      }
+    }
+  }
+  return match
+}
+
+//继承来的HashHistory对象
+export class HashHistory extends Base {
+  constructor (router) {
+    super(router)
+    this.ensureSlash()//主要是为了兼容历史记录，使用replaceState和pushState来记录历史
+    //如果当hash的值发生变化的时候，改变了current的值
+    window.addEventListener('hashchange', () => {
+      this.transitionTo(this.getCurrentLocation())
+    })
+  }
+}
+//继承来的HTML5History对象
+export class HTML5History extends Base {
+  constructor (router) {
+    super(router)
+    //监听当前的历史节点页面加载的时候确实会触发popState事件
+    window.addEventListener('popstate', () => {
+      this.transitionTo(getLocation())
+    })
+  }
+}
+
+
+//router对象
+class Router {
+  constructor(options) {
+    this.base = options.base
+    this.routes = options.routes //路由信息
+    this.container = options.id
+    this.mode = options.mode || 'hash'
+    this.fallback = this.mode === 'history' && !supportsPushState && options.fallback !== false
+    if (this.fallback) {
+      this.mode = 'hash'
+    }
+
+    this.history = this.mode === 'history' ? new HTML5History(this) : new HashHistory(this)
+
+    Object.defineProperty(this, 'route', {
+      get: () => {
+        return this.history.current
+      }
+    })
+
+    this.init()
+  }
+
+  push(location) {
+    this.history.push(location)
+  }
+
+  replace(location) {
+    this.history.replace(location)
+  }
+
+  go (n) {
+    this.history.go(n)
+  }
+
+  render() {
+    let i
+    //这招神了....
+    if ((i = this.history.current) && (i = i.route) && (i = i.component)) {
+      document.getElementById(this.container).innerHTML = i
+    }
+  }
+
+  init() {
+    const history = this.history
+    //监听this.history.current数据
+    observer.call(this, this.history.current)
+    //当this.history.current中的route发生变化的时候执行render函数
+    new Watcher(this.history.current, 'route', this.render.bind(this))
+    //跳转到当前所在的地址
+    history.transitionTo(history.getCurrentLocation())
+  }
+
+}
+
+window.Router = Router
+
+//调用的时候的代码
+
+
+var router = new Router({
+  id: 'router-view',
+  mode: 'hash',
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: '<div>Home</div>',
+      beforeEnter: (next) => {
+        console.log('before enter home')
+        next()
+      },
+      afterEnter: (next) => {
+        console.log('enter home')
+        next()
+      },
+      beforeLeave: (next) => {
+        console.log('start leave home')
+        next()
+      }
+    },
+  ]
+})
